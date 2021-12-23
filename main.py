6db6d2ea14d1b6ec81a463f45646e5b649578163
ToBi22-10 <ilyabogdanov2210@icloud.com>
2021-12-23 16:03:06 +0500

    fixed code

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def main():
+#     return redirect('https://goo.su/aLtG')
+
+
 @app.route('/')
-def main():
-    return redirect('https://goo.su/aLtG')
+def html():
+    return render_template('index.html')
 
 
 @app.route('/ya')
